5d3f8ce36b30e5df2d129ab60ce951bce62cad8e
Alexander Permyakov <pedectrian@gmail.com>
2014-09-30 16:38:48 +0300

    added sidebar template

--- sidebar-exhibition.php
+++ sidebar-exhibition.php
@@ -1,6 +1,6 @@
 				 <!-- Search sidebar -->
 				<aside class="right-aside fourth last">
-
+qqq
 					<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("canon_exhibition_sidebar_widget_area")) : ?>
 						
                         <h4><?php _e("Exhibition Sidebar Widget Area", "loc_canon"); ?></h4>
