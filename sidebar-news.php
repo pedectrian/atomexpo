611119d31afc0fba88bf3b89ad763c99a509d481
Alexander Permyakov <pedectrian@gmail.com>
2014-09-30 16:20:37 +0300

    added sidebar template

--- sidebar-news.php
+++ sidebar-news.php
@@ -1,9 +1,9 @@
 				 <!-- Search sidebar -->
 				<aside class="right-aside fourth last">
 
-					<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("canon_search_sidebar_widget_area")) : ?>  
+					<?php if (!function_exists('dynamic_sidebar')) : ?>
 						
-                        <h4><?php _e("Search Sidebar Widget Area", "loc_canon"); ?></h4>
+                        <h4><?php _e("News Sidebar Widget Area", "loc_canon"); ?></h4>
                         <p><i><?php _e("Please login and add some widgets to this widget area.", "loc_canon"); ?></i></p> 
 					
 			        <?php endif; ?>  
